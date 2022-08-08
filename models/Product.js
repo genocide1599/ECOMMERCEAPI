142c2d486d7745c784f88ad13f34734330198cdc
genocide1599 <61695290+genocide1599@users.noreply.github.com>
2022-08-08 22:10:43 +0800

    7th

--- models/Product.js
+++ models/Product.js
@@ -5,9 +5,10 @@ const ProductSchema = new mongoose.Schema(
         title: {type: String, required: true, unique: true},
         desc: {type: String, required: true},
         img: {type: String, required: true},
-        size: {type: String},
-        color: {type: String},
-        price: {type: Number, required: true}
+        size: {type: Array},
+        color: {type: Array},
+        price: {type: Number, required: true},
+        inStock: {type: Boolean, default: true}
     },
     {timestamps: true}
 );
